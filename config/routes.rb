f0bb535e9ac306b44f9e24222766d485318fa990
Almudena <almudenaprt@gmail.com>
2018-03-26 13:20:39 +0200

    Incluir formulario y modificar las rutas por defecto

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get 'wellcome/index'
+  root 'wellcome#index'
+  
+  get 'index' => 'wellcome#index'
+  post 'index' => 'wellcome#index'
 
-  get 'wellcome/test'
+  get 'test' => 'wellcome#test'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
